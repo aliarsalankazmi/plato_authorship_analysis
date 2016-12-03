e47f5eeb9bc3c6b1b709b1851ea44ff447370864
Ali Arsalan <kazmialiarsalan@gmail.com>
2016-12-03 17:06:47 +0300

    1st Iteration - Extracting data

--- Scripts/data_extraction.R
+++ Scripts/data_extraction.R
@@ -5,6 +5,22 @@ pkgLoaded <- sapply(pkgs, require, character.only =T)
 
 
 
-platoIndex <- 
-  gutenberg_metadata %>%
-    filter(author == "Plato", language == "el")
+platoIndex <- gutenberg_metadata %>%
+                filter(author == "Plato", language == "el")
+
+Sys.setlocale(category = "LC_ALL", locale = "Greek")
+
+
+platoTitles <- platoIndex %>% 
+                  select(gutenberg_id) %>%
+                  gutenberg_download(., strip = F, meta_fields = "title", verbose = T) %>%
+                  filter(grepl("Title", text, ignore.case = T))
+                  group_by(gutenberg_id) %>%
+                  slice(20) %>%
+                  grepl("Title", ignore.case = T)
+
+  
+platoWorks <- platoIndex %>% 
+                select(gutenberg_id) %>%
+                gutenberg_download(., strip = T, meta_fields = "title", verbose = T)
+
